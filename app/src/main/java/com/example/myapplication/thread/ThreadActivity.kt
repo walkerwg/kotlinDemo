b014cabda913a66cf11becbea97b991d2374d488
907029564@qq.com <qq123456>
2021-11-10 17:49:51 +0800

    volatile kotlin 不起作用啊

--- app/src/main/java/com/example/myapplication/thread/ThreadActivity.kt
+++ app/src/main/java/com/example/myapplication/thread/ThreadActivity.kt
@@ -8,14 +8,39 @@ import com.example.myapplication.R
 import kotlin.concurrent.thread
 
 class ThreadActivity : AppCompatActivity() {
+
+     @Volatile private var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thread)
+        Log.i("ThreadActivity", "ssssss" + Thread.currentThread().name)
+
         thread {
             Log.i("ThreadActivity", "dddddd" + Thread.currentThread().name)
         }
-
-
+        testVolatileProperty()
 
     }
+
+
+    private fun testVolatileProperty() {
+
+        thread {
+
+            while (!flag) {
+                Log.i("ThreadActivity", "eeeeee" + Thread.currentThread().name)
+
+            }
+            Log.i("ThreadActivity", "fffff" + Thread.currentThread().name)
+        }
+
+        Thread.sleep(200)
+        thread {
+            flag = true
+            Log.i("ThreadActivity", "ssssss" + Thread.currentThread().name)
+
+        }
+    }
+
+
 }
